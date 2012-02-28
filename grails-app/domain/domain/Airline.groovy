eb285a7c474498e641c19f81f8cadd58287662d5
Marcin Piczkowski <marcin.piczkowski@gmail.com>
2012-02-28 16:22:45 +0100

    Added columns ordering and more user friendly display name (visible in drop-down of Trip)

--- grails-app/domain/domain/Airline.groovy
+++ grails-app/domain/domain/Airline.groovy
@@ -1,9 +1,20 @@
 package domain
 
 class Airline {
+	static constraints = {
+		name()
+		url()
+		frequentFlyer()
+		notes()
+	  }
 	static hasMany = [trip: Trip]
 	String name
 	String url
 	String frequentFlyer
 	String notes
+	
+	String toString() {
+		return name
+	}
+	
 }
