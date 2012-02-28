ba3441685acd9cf2bdf1ea7c373a15d4bc6f01e8
Marcin Piczkowski <marcin.piczkowski@gmail.com>
2012-02-28 20:40:41 +0100

    Valiadtion of properties and changed defailt DB column names

--- grails-app/domain/domain/Airline.groovy
+++ grails-app/domain/domain/Airline.groovy
@@ -1,20 +1,27 @@
 package domain
 
 class Airline {
+	static mapping = {
+		table 'some_other_table_name'
+		columns {
+			name column:'airline_name'
+			url column:'link'
+			frequentFlyer column:'ff_id'
+		}
+	}
 	static constraints = {
-		name()
-		url()
-		frequentFlyer()
-		notes()
-	  }
+		name(blank:false, maxSize:100)
+		url(url:true)
+		frequentFlyer(blank:true)
+		notes(maxSize:1500)
+	}
 	static hasMany = [trip: Trip]
 	String name
 	String url
 	String frequentFlyer
 	String notes
-	
+
 	String toString() {
 		return name
 	}
-	
 }
