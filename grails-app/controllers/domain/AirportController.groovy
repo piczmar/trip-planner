6a10dc1898f5393c1fad513b5495ec860e187472
Marcin Piczkowski <marcin.piczkowski@gmail.com>
2012-03-01 22:00:14 +0100

    Ajax usage in Create Flight form

--- grails-app/controllers/domain/AirportController.groovy
+++ grails-app/controllers/domain/AirportController.groovy
@@ -1,6 +1,21 @@
 package domain
 
+import grails.converters.JSON;
+import grails.converters.XML
+import java.util.TreeMap.AscendingSubMap.AscendingEntrySetView;
+
 class AirportController {
 
 	def scaffold = Airport
+	
+	def getXML = {
+		render Airport.findByIata(params.iata) as XML
+	}
+	def getJson = {
+		def airports = Airport.findByIata(params.iata)
+		if(!airports){
+			airports = new Airport(iata: params.iata , name: "Not found")	
+		}
+		render airports as JSON
+	}
 }
