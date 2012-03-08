dff4c8a9a2e5b70c66798933d91cdc8da42bc8bc
Marcin Piczkowski <marcin.piczkowski@gmail.com>
2012-03-08 02:32:47 +0100

    Reading JMX properties from groovy script

---
+++ jmx/testJMX.groovy
@@ -0,0 +1,16 @@
+import javax.management.MBeanServerConnection
+import javax.management.remote.JMXConnectorFactory
+import javax.management.remote.JMXServiceURL
+
+def agentUrl = "service:jmx:rmi:///jndi/rmi://localhost:9004/jmxrmi"
+def connector = JMXConnectorFactory.connect(new JMXServiceURL(agentUrl))
+def server = connector.getMBeanServerConnection()
+//def server = connector.mBeanServerConnection // NOTE: does not work in Grails 1.3, replaced with previous line
+
+println "Number of registered MBeans: ${server.getMBeanCount()}"
+
+println "\nRegistered Domains:"
+server.domains.each{println it}
+
+println "\nRegistered MBeans:"
+server.queryNames(null, null).each{println it}
