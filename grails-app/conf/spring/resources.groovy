0e1d7e942e43b0828b2efad41537bbb2ddad5a51
Marcin Piczkowski <marcin.piczkowski@gmail.com>
2012-03-08 02:17:58 +0100

    Replaced XML JMX configuration with .groovy

--- grails-app/conf/spring/resources.groovy
+++ grails-app/conf/spring/resources.groovy
@@ -1,3 +1,14 @@
-// Place your Spring DSL code here
+import org.springframework.jmx.support.MBeanServerFactoryBean
+import org.springframework.jmx.export.MBeanExporter
+import org.apache.log4j.jmx.HierarchyDynamicMBean
+
 beans = {
+	log4jBean(HierarchyDynamicMBean)
+
+	mbeanServer(MBeanServerFactoryBean) { locateExistingServerIfPossible=true }
+
+	exporter(MBeanExporter) {
+		server = mbeanServer
+		beans = ["log4j:hierarchy=default":log4jBean]
+	}
 }
