5b1ba529f51c7ba11c59db5f3838567bdd5b1059
Marcin Piczkowski <marcin.piczkowski@gmail.com>
2012-03-08 02:19:11 +0100

    Different JMX configuration for development and prod. envronments

--- grails-app/conf/spring/resources.groovy
+++ grails-app/conf/spring/resources.groovy
@@ -1,14 +1,22 @@
 import org.springframework.jmx.support.MBeanServerFactoryBean
 import org.springframework.jmx.export.MBeanExporter
 import org.apache.log4j.jmx.HierarchyDynamicMBean
+import grails.util.GrailsUtil
 
 beans = {
 	log4jBean(HierarchyDynamicMBean)
 
 	mbeanServer(MBeanServerFactoryBean) { locateExistingServerIfPossible=true }
 
-	exporter(MBeanExporter) {
-		server = mbeanServer
-		beans = ["log4j:hierarchy=default":log4jBean]
+	switch(GrailsUtil.environment){
+		case "development":
+			break
+
+		case "production":
+			exporter(MBeanExporter) {
+				server = mbeanServer
+				beans = ["log4j:hierarchy=default":log4jBean]
+			}
+			break
 	}
 }
